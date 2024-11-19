e818c88f9fca0a3fa1fe9bf565253d55a16fb86b
Smoshed <j.a.oltmans@gmail.com>
2024-11-19 13:43:01 +0100

    Add files via upload

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -2,9 +2,16 @@
 
 function university_post_types() {
     register_post_type('event', array(
+      'supports' => array('title', 'editor', 'excerpt',),
+      'rewrite' => array('slug' => 'events'),
+      'has_archive' => true,
       'public' =>  true,
       'labels' => array(
-        'name' => 'Events'
+        'name' => 'Events',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event'
       ),
       'menu_icon' => 'dashicons-calendar'
     ));
